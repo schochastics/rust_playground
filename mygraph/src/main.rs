4a4025a7b1ed526e4e6377677caeefa3c6aed5a2
schochastics <david@schochastics.net>
2024-02-10 18:22:48 +0100

    implemented dijkstra for unit cost

--- mygraph/src/main.rs
+++ mygraph/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 struct Graph {
     adj_list: Vec<Vec<usize>>,
     vertices: usize,
@@ -11,16 +13,9 @@ impl Graph {
     }
 
     // Add an edge between two vertices
-    fn add_edge(&mut self, v1: usize, v2: usize) {
-        self.adj_list[v1].push(v2);
-        self.adj_list[v2].push(v1); // Because it's an undirected graph
-    }
-
-    // Print the adjacency list (for debugging purposes)
-    fn print(&self) {
-        for (i, neighbors) in self.adj_list.iter().enumerate() {
-            println!("Vertex {}: {:?}", i, neighbors);
-        }
+    fn add_edge(&mut self, src: usize, des: usize) {
+        self.adj_list[src].push(des);
+        self.adj_list[des].push(src); // Because it's an undirected graph
     }
 
     fn degree(&self) -> Vec<usize> {
@@ -28,6 +23,32 @@ impl Graph {
             .iter()
             .map(|neighbors| neighbors.len())
             .collect()
+    }
+
+    fn dijkstra(&self, src: usize) -> Vec<usize> {
+        let mut distances = vec![usize::MAX; self.vertices];
+        let mut queue = VecDeque::new();
+        let mut visited = HashSet::new();
+
+        distances[src] = 0;
+        queue.push_back(src);
+        visited.insert(src);
+
+        while let Some(u) = queue.pop_front() {
+            for &v in &self.adj_list[u] {
+                if !visited.contains(&v) {
+                    visited.insert(v);
+                    queue.push_back(v);
+                    distances[v] = distances[u] + 1; // Since each edge cost is considered 1
+                }
+            }
+        }
+
+        distances
+    }
+
+    fn distances(&self) -> Vec<Vec<usize>> {
+        (0..self.vertices).map(|src| self.dijkstra(src)).collect()
     }
 }
 
@@ -42,11 +63,11 @@ fn main() {
     graph.add_edge(2, 3);
     graph.add_edge(3, 4);
 
-    graph.print(); // Print the adjacency list to see the graph structure
-
     // Calculate and print degrees
     let degrees = graph.degree();
     for (node, degree) in degrees.iter().enumerate() {
         println!("Node {}: Degree {}", node, degree);
     }
+
+    let dist_mat = graph.distances();
 }
